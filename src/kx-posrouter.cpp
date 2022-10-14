38b5a9aa01a74ae1e9d0e392e937304cff2fcbcc
fluffysnaff <fluffysnaff@protonmail.com>
2022-10-14 17:48:06 +0200

    Complete the converting of json files + switching y z

--- src/kx-posrouter.cpp
+++ src/kx-posrouter.cpp
@@ -6,108 +6,76 @@
 
 int main()
 {
-	/*
-	 *
-{
-    "name": "example name",
-    "description": "example description",
-    "mapId": 15,
-    "author": "me",
-    "coordinates": [
-        {
-            "name": "example name1",
-            "x": 100.00,
-            "y": 100.00,
-            "z": 100.00
-        },
-        {
-            "name": "example name2",
-            "x": 200.00,
-            "y": 200.00,
-            "z": 200.00
-        },
-        {
-            "name": "example name3",
-            "x": 300.00,
-            "y": 300.00,
-            "z": 300.00
-        }
-    ]
-}
-	json j = {
-		{"name", "example name"},
-		{"coordinates", {{"name", "example name"}, {"x", 100.00}, {"y", 100.00}, {"z", 100.00}}, {{"name", "example name2"}, {"x", 200.00}, {"y", 200.00}, {"z", 200.00}}, {{"name", "example name3"}, {"x", 300.00}, {"y", 300.00}, {"z", 300.00}}}
+	std::string srcDirectory;
+	spdlog::info("Enter the path of the source directory: ");
+	std::getline(std::cin, srcDirectory);
+	srcDirectory = std::filesystem::path(srcDirectory).generic_string();
+
+	if (!std::filesystem::is_directory(srcDirectory))
+	{
+		spdlog::error("Path '{}' is invalid", srcDirectory);
+		return 1;
 	}
-	 */
-	// std::string pathToDirectory;
-	// std::string printChangedLinesStr = "n";
-	// bool printChangedLines;
-	// spdlog::info("Enter the path of the directory: ");
-	// std::getline(std::cin, pathToDirectory);
-	// if (!std::filesystem::is_directory(pathToDirectory))
-	// {
-	// 	spdlog::error("Path '{}' is invalid", pathToDirectory);
-	// 	return 1;
-	// }
-	// 
-	// spdlog::info("Print changed lines?(y/n): ");
-	// std::cin >> printChangedLinesStr;
-	// printChangedLines = printChangedLinesStr == "n" ? false : true;
-	// 
-	// auto t1 = std::chrono::high_resolution_clock::now();
-	// const std::vector<std::string> filesInDir = Utils::GetFilesInDirectory(pathToDirectory);
-	// for (const std::string fileName : filesInDir)
-	// {
-	// 	if (fileName.find("map_names.json") != std::string::npos)
-	// 		continue;
-	// 
-	// 	// My lines
-	// 	std::vector<std::string> lines = Utils::ReadLines(fileName);
-	// 	for (const std::string line : lines)
-	// 	{
-	// 		const std::string firstPart = Utils::GetFirstPart(line);
-	// 		const std::vector<double> pos = Utils::GetPosition(line, fileName);
-	// 		if (pos.empty())
-	// 			return 1;
-	// 		const std::vector<double> newPos = Utils::SwitchYZ(pos);
-	// 		if (newPos.empty())
-	// 			return 1;
-	// 		const std::string secondPart = Utils::ConvertSecondPart(newPos);
-	// 		const std::string final = firstPart + secondPart;
-	// 		if (printChangedLines)
-	// 			spdlog::info("{}, {}, {}", fileName, final, line);
-	// 	}
-	// }
-	// auto t2 = std::chrono::high_resolution_clock::now();
-	// spdlog::info("Program took {} {} to execute", std::chrono::duration_cast<std::chrono::seconds>(t2 - t1).count(),
-	//              "seconds");
-	Utils::CreateNewFile("test.json");
-	// nlohmann::json j = {
-	// {"name", "example name"},
-	// {"coordinates", {{"name", "example name"}, {"x", 100.00}, {"y", 100.00}, {"z", 100.00}}, {{"name", "example name2"}, {"x", 200.00}, {"y", 200.00}, {"z", 200.00}}, {{"name", "example name3"}, {"x", 300.00}, {"y", 300.00}, {"z", 300.00}}}
-	// };
+	
+	spdlog::info("Print changed lines?(y/n): ");
 
+	// Get directory path and remove the last dir, then append new_maps
+	std::string directoryPath = srcDirectory.substr(0, srcDirectory.find_last_of("\\/"));
+	directoryPath.append("/new_maps");
+	std::filesystem::create_directories(directoryPath);
 
-	// nlohmann::json j = nlohmann::json
-	// {
-	// 	{"name", "Example name"},
-	// 	{"Coordinates", result},
-	// 	{"test", "1"}
-	// };
-	nlohmann::ordered_json result = nlohmann::json::array();
-	std::vector<Models::Coordinates> coords;
-	nlohmann::ordered_json finalJson;
+	auto t1 = std::chrono::high_resolution_clock::now();
+	const std::vector<std::string> filesInDir = Utils::GetFilesInDirectory(srcDirectory);
+	for (const std::string fileName : filesInDir)
+	{
+		if (fileName.find("map_names.json") != std::string::npos)
+			continue;
 
-	coords.push_back(Models::Coordinates{ "example name", 1, 1, 1 });
-	Models::vector_to_json(result, coords);
-	Models::Map newMap;
-	newMap.coords = result;
-	newMap.name = "Example name";
+		std::string midPath = fileName;
+		Utils::RemoveString(midPath, std::filesystem::path(fileName).filename().generic_string());
+		Utils::RemoveString(midPath, srcDirectory);
 
-	Models::to_json(finalJson, newMap);
+		// Create a file with with the filename path + json
+		if(!std::filesystem::is_directory(directoryPath + midPath) || !std::filesystem::exists(directoryPath + midPath))
+		{
+			std::filesystem::create_directories(directoryPath + midPath);
+		}
 
-	std::ofstream f("test.json", std::ios_base::trunc | std::ios_base::out);
-	f << std::setw(4) << finalJson << std::endl;
+		std::string file = directoryPath + midPath + std::filesystem::path(fileName).stem().generic_string() + ".json";
+		if (!Utils::CreateNewFile(file))
+			break;
+	
+		// My lines
+		std::vector<std::string> lines = Utils::ReadLines(fileName);
+
+		nlohmann::ordered_json resultCoords;
+		std::vector<Models::Coordinates> coords;
+		nlohmann::ordered_json finalJson;
+		for (const std::string line : lines)
+		{
+			const std::vector<double> pos = Utils::GetPosition(line, fileName);
+			if (pos.empty())
+				return 1;
+			const std::vector<double> newPos = Utils::SwitchYZ(pos);
+			if (newPos.empty())
+				return 1;
+			const std::string name = Utils::GetCheckpointName(line);
+
+			coords.push_back(Models::Coordinates{ name, newPos[0], newPos[1], newPos[2]});
+		}
+		Models::vector_to_json(resultCoords, coords);
+		Models::Map newMap;
+		newMap.coords = resultCoords;
+		newMap.name = std::filesystem::path(fileName).stem().generic_string();
+
+		Models::to_json(finalJson, newMap);
+
+		std::ofstream f(file, std::ios_base::trunc | std::ios_base::out);
+		f << finalJson.dump(4, ' ', false, nlohmann::json::error_handler_t::ignore) << std::endl;
+	}
+	auto t2 = std::chrono::high_resolution_clock::now();
+	spdlog::info("Program took {} {} to execute", std::chrono::duration_cast<std::chrono::seconds>(t2 - t1).count(),
+	             "seconds");
 	return 0;
 }
 
