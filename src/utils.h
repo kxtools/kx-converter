fd84ec22de2f30f4d74ec8f65fc7b763962ad758
fluffysnaff <fluffysnaff@protonmail.com>
2022-10-15 16:06:00 +0200

    Changed strings that can use generic_string to utf-8

--- src/utils.h
+++ src/utils.h
@@ -229,7 +229,7 @@ namespace Utils
 		std::vector<std::string> out;
 		for (std::filesystem::recursive_directory_iterator i(directory), end; i != end; ++i)
 			if (!is_directory(i->path()))
-				out.push_back(i->path().generic_string());
+				out.push_back(i->path().generic_u8string());
 		return out;
 	}
 
